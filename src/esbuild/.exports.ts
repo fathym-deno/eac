877d9ea16b43b54d96aa33e2d2894906002d411f
mcgear <michael.gearhardt@gmail.com>
2024-11-15 02:47:18 -0700

    more esbuild exports

--- src/esbuild/.exports.ts
+++ src/esbuild/.exports.ts
@@ -24,6 +24,22 @@ export type ESBuild = {
 
 export type ESBuildOptions = esbuild.BuildOptions;
 
+export type ESBuildOnLoadResult = esbuild.OnLoadResult;
+
+export type ESBuildOnLoadArgs = esbuild.OnLoadArgs;
+
+export type ESBuildOnResolveResult = esbuild.OnResolveResult;
+
+export type ESBuildOnResolveArgs = esbuild.OnResolveArgs;
+
+export type ESBuildPlugin = esbuild.Plugin;
+
+export type ESBuildPluginBuild = esbuild.PluginBuild;
+
 export type ESBuildContext<
   ProvidedOptions extends ESBuildOptions = ESBuildOptions,
 > = esbuild.BuildContext<ProvidedOptions>;
+
+export type ESBuildResult<
+  ProvidedOptions extends ESBuildOptions = ESBuildOptions,
+> = esbuild.BuildResult<ProvidedOptions>;
