349fb83e6dce091fe1ad9cc71fac6b8914bbafb2
mcgear <michael.gearhardt@gmail.com>
2025-02-13 00:10:06 -0700

    ref updates

--- src/runtime/pipelines/.deps.ts
+++ src/runtime/pipelines/.deps.ts
@@ -1,3 +1,3 @@
-export type { KnownMethod } from "jsr:@fathym/common@0.2.173/http";
+export type { KnownMethod } from "jsr:@fathym/common@0.2.175/http";
 
 export type { EaCRuntimeContext } from "../_/.exports.ts";
