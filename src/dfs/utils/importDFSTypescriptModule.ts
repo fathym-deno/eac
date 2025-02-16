23673ae2f0a98522035417bc0ae8b9239a7386c1
mcgear <michael.gearhardt@gmail.com>
2025-02-16 01:34:34 -0700

    thing

--- src/dfs/utils/importDFSTypescriptModule.ts
+++ src/dfs/utils/importDFSTypescriptModule.ts
@@ -26,65 +26,54 @@ export async function importDFSTypescriptModule(
     );
 
     if (file) {
-      let fileContents = await toText(file!.Contents);
+      try {
+        let fileContents = await toText(file!.Contents);
 
-      if (loader === "tsx") {
-        fileContents = `import { Fragment, h } from "preact";\n${fileContents}`;
-      }
-
-      let apiUrl: string;
-
-      // if (IS_DENO_DEPLOY()) {
-      //   const result = await esbuild.transform(fileContents, {
-      //     loader: loader,
-      //     // jsx: 'react-jsx',
-      //     jsxImportSource: 'preact',
-      //     jsxFactory: 'h',
-      //     jsxFragment: 'Fragment',
-      //     platform: 'browser',
-      //   });
-
-      //   // const enc = base64.encodeBase64(fileContents);
-      //   const enc = base64.encodeBase64(result.code);
-
-      //   // const apiUrl = `data:application/typescript;base64,${enc}`;
-      //   apiUrl = `data:application/javascript;base64,${enc}`;
-      // } else {
-
-      filePath = file.Path;
-
-      if (filePath.startsWith("http://") || filePath.startsWith("https://")) {
-        apiUrl = filePath;
-      } else if (
-        fileHandler.Root.startsWith("http://") ||
-        fileHandler.Root.startsWith("https://")
-      ) {
-        if (filePath.startsWith("/")) {
-          filePath = `.${filePath}`;
+        if (loader === "tsx") {
+          fileContents =
+            `import { Fragment, h } from "preact";\n${fileContents}`;
         }
 
-        apiUrl = new URL(`${filePath}`, fileHandler.Root).href;
-      } else {
-        if (filePath.startsWith("file:")) {
+        let apiUrl: string;
+
+        filePath = file.Path;
+
+        if (filePath.startsWith("http://") || filePath.startsWith("https://")) {
           apiUrl = filePath;
+        } else if (
+          fileHandler.Root.startsWith("http://") ||
+          fileHandler.Root.startsWith("https://")
+        ) {
+          if (filePath.startsWith("/")) {
+            filePath = `.${filePath}`;
+          }
+
+          apiUrl = new URL(`${filePath}`, fileHandler.Root).href;
         } else {
-          apiUrl = `file:///${
-            path.join(
-              fileHandler.Root.includes(":/") ||
-                fileHandler.Root.includes(":\\")
-                ? ""
-                : Deno.cwd(),
-              fileHandler.Root,
-              filePath,
-            )
-          }`;
+          if (filePath.startsWith("file:")) {
+            apiUrl = filePath;
+          } else {
+            apiUrl = `file:///${
+              path.join(
+                fileHandler.Root.includes(":/") ||
+                  fileHandler.Root.includes(":\\")
+                  ? ""
+                  : Deno.cwd(),
+                fileHandler.Root,
+                filePath,
+              )
+            }`;
+          }
         }
+        // }
+
+        const module = await import(apiUrl);
+
+        return { filePath: apiUrl, module, contents: fileContents };
+      } catch (ex) {
+        console.log(file!.Path);
+        throw ex;
       }
-      // }
-
-      const module = await import(apiUrl);
-
-      return { filePath: apiUrl, module, contents: fileContents };
     } else {
       return undefined;
     }
