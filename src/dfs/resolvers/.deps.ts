49c3c826091b794b7cc3aa9b9e7cc1d82e2b859d
mcgear <michael.gearhardt@gmail.com>
2025-12-04 00:55:53 -0700

    ref updates

--- src/dfs/resolvers/.deps.ts
+++ src/dfs/resolvers/.deps.ts
@@ -8,15 +8,15 @@ export { IoCContainer } from "jsr:@fathym/ioc@0.0.25";
 export {
   type DFSFileInfo,
   type IDFSFileHandler,
-} from "jsr:@fathym/dfs@0.0.59-integration";
+} from "jsr:@fathym/dfs@0.0.62-integration";
 export {
   CompositeDFSFileHandler,
   LocalDFSFileHandler,
-} from "jsr:@fathym/dfs@0.0.59-integration/handlers";
+} from "jsr:@fathym/dfs@0.0.62-integration/handlers";
 export {
   ESMFetchDFSFileHandler,
   FetchDFSFileHandler,
   JSRFetchDFSFileHandler,
   NPMFetchDFSFileHandler,
   RemoteFetchDFSFileHandler,
-} from "jsr:@fathym/dfs@0.0.59-integration/handlers/fetch";
+} from "jsr:@fathym/dfs@0.0.62-integration/handlers/fetch";
--- src/dfs/utils/.deps.ts
+++ src/dfs/utils/.deps.ts
@@ -10,6 +10,6 @@ export {
   DFSFileHandler,
   type DFSFileInfo,
   type IDFSFileHandler,
-} from "jsr:@fathym/dfs@0.0.59-integration";
+} from "jsr:@fathym/dfs@0.0.62-integration";
 
 export { IoCContainer } from "jsr:@fathym/ioc@0.0.25";
