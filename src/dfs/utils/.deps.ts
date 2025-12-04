3401c1f2e9697e966792369aefc026b42eab93e5
mcgear <michael.gearhardt@gmail.com>
2025-12-04 00:38:08 -0700

    fixes to misimplemented DFS

--- src/dfs/utils/.deps.ts
+++ src/dfs/utils/.deps.ts
@@ -10,6 +10,6 @@ export {
   DFSFileHandler,
   type DFSFileInfo,
   type IDFSFileHandler,
-} from "jsr:@fathym/dfs@0.0.58-integration";
+} from "jsr:@fathym/dfs@0.0.59-integration";
 
 export { IoCContainer } from "jsr:@fathym/ioc@0.0.25";
