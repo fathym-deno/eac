d9399f8f08d3ea0590f31d1a0c2cf7102325ee61
mcgear <michael.gearhardt@gmail.com>
2025-12-05 11:41:32 -0700

    ref updates

--- src/dfs/utils/.deps.ts
+++ src/dfs/utils/.deps.ts
@@ -10,6 +10,6 @@ export {
   DFSFileHandler,
   type DFSFileInfo,
   type IDFSFileHandler,
-} from "jsr:@fathym/dfs@0.0.72-integration";
+} from "jsr:@fathym/dfs@0.0.73-integration";
 
 export { IoCContainer } from "jsr:@fathym/ioc@0.0.25";
